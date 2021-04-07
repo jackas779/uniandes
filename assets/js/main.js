6dca18b0f11e2821d3294a90a5aa9a24202d7266
Nicolas Ardila <jackas779@gmail.com>
2021-04-07 16:53:10 -0500

    :lipstick: se agrega sub menú para versión mobil y se corrige el media query del super menú para desktop-xl y desktop

--- assets/js/main.js
+++ assets/js/main.js
@@ -51,7 +51,7 @@ function invisible_card() {
 }
  
 
-if (screen.width <= 780){
+if (screen.width >= 780){
   alert("La resolución de tu pantalla es de un mobil " )
 }
 else{
@@ -93,7 +93,7 @@ menu_3.addEventListener('mouseout',()=>{
 })
 }
 
-
+if (screen.width <= 490){
 let burger = document.querySelector('.menu_uniandes_burger__img__2')
 burger.addEventListener('click',left)
 var contad= 0
@@ -101,11 +101,144 @@ var contad= 0
 function left(){
   if(contad==0){
   let menu = document.querySelector('.header_uniandes')
+  menu.classList.add("activo")
   menu.style.left='0'
   contad=1
   }else{
     let menu = document.querySelector('.header_uniandes')
     menu.style.removeProperty("left");
+    let container__1 = document.querySelector('.items_container_1')
+    container__1.style.removeProperty("left");
+    let container__2 = document.querySelector('.items_container_2')
+    container__2.style.removeProperty("left");
+    let container__3 = document.querySelector('.items_container_3')
+    container__3.style.removeProperty("left");
     contad=0
   }
 }    
+
+let menu_1 = document.querySelector('.menu_uniandes__links').childNodes[5]
+let menu_2 = document.querySelector('.menu_uniandes__links').childNodes[9]
+let menu_3 = document.querySelector('.menu_uniandes__links').childNodes[11]
+let sub_menu_ce_01 = document.querySelector('#cerrar_sub_menu_01')
+let sub_menu_ce_011 = document.querySelector('#cerrar_sub_menu_011')
+let sub_menu_ce_0111 = document.querySelector('#cerrar_sub_menu_0111')
+let sub_menu_ce_02 = document.querySelector('#cerrar_sub_menu_02')
+let sub_menu_ce_022 = document.querySelector('#cerrar_sub_menu_022')
+let sub_menu_ce_03 = document.querySelector('#cerrar_sub_menu_03')
+let avance_sub_menu_01 = document.querySelector('#avance_sub_menu_01')
+let avance_sub_menu_011 = document.querySelector('#avance_sub_menu_011')
+let avance_sub_menu_02 = document.querySelector('#avance_sub_menu_02')
+
+sub_menu_ce_01.addEventListener('click',()=>{
+  let container__1 = document.querySelector('.items_container_1')
+  container__1.style.removeProperty("left");
+})
+
+sub_menu_ce_011.addEventListener('click',()=>{
+  let container__2 = document.querySelector('.items_container_2')
+  container__2.style.removeProperty("left");
+})
+
+sub_menu_ce_0111.addEventListener('click',()=>{
+  let container__2 = document.querySelector('.items_container_3')
+  container__2.style.removeProperty("left");
+})
+
+sub_menu_ce_02.addEventListener('click',()=>{
+  let content__2 = document.querySelector('.items_container_1').childNodes[3]
+  let content__1 = document.querySelector('.items_container_1').childNodes[1]
+  content__1.classList.remove("att")
+  content__1.classList.add("atr")
+  content__2.classList.remove("atr")
+  content__2.classList.add("att")
+})
+sub_menu_ce_022.addEventListener('click',()=>{
+  let content__2 = document.querySelector('.items_container_2').childNodes[3]
+  let content__1 = document.querySelector('.items_container_2').childNodes[1]
+  content__1.classList.remove("att")
+  content__1.classList.add("atr")
+  content__2.classList.remove("atr")
+  content__2.classList.add("att")
+})
+
+sub_menu_ce_03.addEventListener('click',()=>{
+  let content__3 = document.querySelector('.items_container_1').childNodes[5]
+  let content__2 = document.querySelector('.items_container_1').childNodes[3]
+  content__2.classList.remove("att")
+  content__2.classList.add("atr")
+  content__3.classList.remove("atr")
+  content__3.classList.add("att")
+})
+
+avance_sub_menu_01.addEventListener('click',()=>{
+  let content__2 = document.querySelector('.items_container_1').childNodes[3]
+  let content__1 = document.querySelector('.items_container_1').childNodes[1]
+  content__1.classList.remove("atr")
+  content__1.classList.add("att")
+  content__2.classList.remove("att")
+  content__2.classList.add("atr")
+})
+
+avance_sub_menu_011.addEventListener('click',()=>{
+  let content__2 = document.querySelector('.items_container_2').childNodes[3]
+  let content__1 = document.querySelector('.items_container_2').childNodes[1]
+  content__1.classList.remove("atr")
+  content__1.classList.add("att")
+  content__2.classList.remove("att")
+  content__2.classList.add("atr")
+})
+
+avance_sub_menu_02.addEventListener('click',()=>{
+  let content__2 = document.querySelector('.items_container_1').childNodes[3]
+  let content__3 = document.querySelector('.items_container_1').childNodes[5]
+  content__2.classList.remove("atr")
+  content__2.classList.add("att")
+  content__3.classList.remove("att")
+  content__3.classList.add("atr")
+})
+
+menu_1.addEventListener('click',()=>{
+  let conta_1= 0;
+  if(conta_1==0){
+    let container__1 = document.querySelector('.items_container_1')
+  container__1.style.left='0'
+  let content__1 = document.querySelector('.items_container_1').childNodes[1]
+  content__1.classList.add("atr")
+  }else{
+    let content__1 = document.querySelector('.items_container_1').childNodes[1]
+    content__1.classList.remove("atr")
+    content__1.classList.add("att")
+  }
+  
+})
+menu_2.addEventListener('click',()=>{
+  let conta_1= 0;
+  if(conta_1==0){
+    let container__2 = document.querySelector('.items_container_2')
+  container__2.style.left='0'
+  let content__1 = document.querySelector('.items_container_1').childNodes[1]
+  content__1.classList.add("atr")
+  }else{
+    let content__1 = document.querySelector('.items_container_1').childNodes[1]
+    content__1.classList.remove("atr")
+    content__1.classList.add("att")
+  }
+  
+})
+
+menu_3.addEventListener('click',()=>{
+  let conta_1= 0;
+  if(conta_1==0){
+    let container__2 = document.querySelector('.items_container_3')
+  container__2.style.left='0'
+  let content__1 = document.querySelector('.items_container_1').childNodes[1]
+  content__1.classList.add("atr")
+  }else{
+    let content__1 = document.querySelector('.items_container_1').childNodes[1]
+    content__1.classList.remove("atr")
+    content__1.classList.add("att")
+  }
+  
+})
+}
